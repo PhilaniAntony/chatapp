303e990ab54404c173b49e2737f4527fb9356aa8
PhilaniAntony <philaniantonydlamini@gmail.com>
2021-01-12 10:47:49 +0200

    add

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '^)%22*pzzi*&g@vs3t5@_)tsj0$tqt8ry5b61e-mh=gdfh3mvg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['chatapplc.herokuapp.com', '127.0.0.1:8000']
 
 
 # Application definition
